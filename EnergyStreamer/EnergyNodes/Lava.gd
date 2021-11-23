494c989ac9d85b71253c6d808da2a0d44ec463af
Norodix <peceli.bence@gmail.com>
2021-11-23 20:44:39 +0100

    Added new lava mechanic

---
+++ EnergyStreamer/EnergyNodes/Lava.gd
@@ -0,0 +1,24 @@
+tool
+extends Area2D
+
+
+export(float, 0, 1) var E = 1 #Energy
+export(float, 1, 10) var maxE = 1
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$Sprite.modulate.g = 0;
+	$Sprite.modulate.b = 0;
+	pass # Replace with function body.
+
+
+func _process(delta):
+	$Sprite.modulate.r = 0.2 + E
+	var frozen = E < 0.01
+	#Disable damage when the lava is frozen
+	$DamageZone/CollisionShape2D.disabled = frozen
+	#Enable walking when frozen
+	$WalkZone/CollisionShape2D.disabled = !frozen
+	if frozen:
+		$Sprite.modulate.r = 0
+	pass
--- EnergyStreamer/Player.gd
+++ EnergyStreamer/Player.gd
@@ -60,7 +60,7 @@ func _physics_process(delta):
 		velocity.x = sign(velocity.x) * max_speed
 
 	velocity.y += gravity * delta
-	var snap = Vector2.DOWN * 64 if is_on_floor() else Vector2.ZERO
+	var snap = Vector2.DOWN * 32 if is_on_floor() else Vector2.ZERO
 	#velocity = move_and_slide(velocity, Vector2.UP)
 	if Input.is_action_pressed("jump"):
 		if ground:
