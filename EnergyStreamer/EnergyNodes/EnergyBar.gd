f39f9d5ff264f5a82c7419693b31688a35e7dcea
Norodix <peceli.bence@gmail.com>
2021-11-25 21:48:16 +0100

    Energybar added to energy nodes

---
+++ EnergyStreamer/EnergyNodes/EnergyBar.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+var parent
+var fullSize = 30
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	parent = get_parent()
+	if parent.get("E") == null:
+		parent = null
+	pass # Replace with function body.
+
+func _process(delta):
+	if parent != null:
+		var size = fullSize * parent.E / parent.maxE
+		$fg.rect_size = Vector2(size, 6)
+		pass
