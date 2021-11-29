f929c778880225d436dfb0a823ac3fc1292ea3cc
Norodix <peceli.bence@gmail.com>
2021-11-29 19:09:15 +0100

    small script adjustment

--- EnergyStreamer/Export/upload.sh
+++ EnergyStreamer/Export/upload.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cd "${0%/*}" #must change to own directory for bash
 make
 butler push HTML.zip Norodix/energystreamer:html
 butler push Android/EnergyStreamer.apk Norodix/energystreamer:android
