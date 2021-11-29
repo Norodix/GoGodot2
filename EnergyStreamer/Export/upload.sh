956cd023918107d72bcddb25724c013d48aed0ce
Norodix <peceli.bence@gmail.com>
2021-11-29 22:01:03 +0100

    Uploads for linux and win builds automated

--- EnergyStreamer/Export/upload.sh
+++ EnergyStreamer/Export/upload.sh
@@ -4,3 +4,5 @@ cd "${0%/*}" #must change to own directory for bash
 make
 butler push HTML.zip Norodix/energystreamer:html
 butler push Android/EnergyStreamer.apk Norodix/energystreamer:android
+butler push EnergyStreamer_win.zip Norodix/energystreamer:windows
+butler push EnergyStreamer_linux.zip Norodix/energystreamer:linux
